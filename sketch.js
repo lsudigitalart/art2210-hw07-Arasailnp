0eabc163dc5bcbe6ddf64fd9a8cd25efe1182973
Arasailnp <Splain3@lsu.edu>
2024-12-12 13:44:29 -0600

    nasa

--- sketch.js
+++ sketch.js
@@ -1,77 +1,58 @@
-let generateCreature = false;
-let creatureW = 0;
-let creatureH = 0;
-let creatureM = 50;
-let eyeSize = 35;
-let mustacheStroke =25
-function preload (){
-  eye1 = loadImage ("file.png")
-  eye2 = loadImage ("file1.png")
-}
-function setup() {
-  createCanvas(400, 400);
-}
+var nasa
+var playTime, loadTime
+var amp, level
+var bgcolor
+var fft
 
-function draw() {
-  background(220);
-  translate(width/2, height/2 )
+function preload(){
  
-  if(generateCreature){
-    creatureBuilder(creatureW, creatureH, creatureM);
+  nasa= loadSound("nasa.mp3")
+  bg = loadImage("cover.jpg")
 }
+
+function setup(){
+  createCanvas(1200, 1200)
+  
+    if (nasa.isLoaded()){
+      loadTime = millis()
+      nasa.play()
+    }
+    amp = new p5.Amplitude()
+    fft = new p5.FFT()
 }
-function creatureBuilder( creatureWidth, creatureHeight, creatureMouth ){
-  //body
-  fill(200,10,450)
+
+function draw(){
+  image(bg,0,0,1200,1200)
+  playTime = millis() - loadTime
+  level = amp.getLevel()
+  
+  push();
+  let waveform1 = fft.waveform();
+  noFill();
+  beginShape();
+  stroke("300");
+  strokeCap (PROJECT)
+  strokeWeight(10);
+  for (var i = 0; i< waveform1.length; i++){
+    let x = map(i, 0, waveform1.length / 100, 0, width);
+    let y = map( waveform1[i], -1, 1, 10, height*3);
+    vertex(x ,y/3);
+  }
+  endShape();
+  pop();
+  
+  var spectrum = fft.analyze();
+  var trebleVol = fft.getEnergy("treble");
+  var midVol = fft.getEnergy("mid");
+  var bassVol = fft.getEnergy("bass");
+
+  fill(30)
   noStroke()
-  ellipse(0,0,creatureHeight,creatureWidth)
-  
-  
-  //Eyes
-   fill(0)
-  image(eye1, -creatureWidth / 8 - eyeSize / 2, -creatureHeight / 4 - eyeSize / 2, eyeSize, eyeSize);
-  image(eye2, creatureWidth / 8 - eyeSize / 2, -creatureHeight / 4 - eyeSize / 2, eyeSize, eyeSize);
-
-   circle(0, creatureHeight/4, creatureMouth);
-  
-  //mustache 
-  stroke(255)
-  strokeWeight(3)
-   line(-creatureWidth/3 - mustacheStroke, -creatureHeight/4 +mustacheStroke,5,5);
-  line (creatureWidth/3 + mustacheStroke , -creatureHeight/8 - mustacheStroke,5,5);
-  
-  //cheeks
-  fill(450,50,150)
-  noStroke()
-  circle(creatureWidth/3, -creatureHeight/12, 25)
-  circle(-creatureWidth/3, -creatureHeight/12, 25)
-   
-}
+square(250 / 2, height - (250 / 2), trebleVol);
+  square(width / 2, height - (250 / 2), midVol);
+  square(width-150 / 2, height - (250 / 2), bassVol);
 
 
-function mousePressed() {
-  generateCreature = true;
-  
-  if(mouseX > width/2) {
-    creatureM = random(25, 50);
-
-  }
-  if(mouseX < width/2) {
-    creatureM = random(0, 25);
-  }
-
-  creatureW = 250
-
-  if(mouseY < height/2) {
-    creatureH = random(250, 500)
-  }
-
-  if(mouseY > height/2) {
-    creatureH = random(50, 250)
-  }
-
-  eyeSize = random(30,120);
-  mustacheStroke = random(80);
-  
 
 }
+
